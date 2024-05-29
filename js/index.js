cbd5d718ac2b9db6b474d73c9d9f8130c018a064
Minje Sung <minje813@dgu.ac.kr>
2024-05-30 00:16:37 +0900

    [fix] index의 link-card 오류 수정

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // Link-Card 연결
 for (let i = 0; i < links.length; i++) {
     let link = links[i]
-    $('.link-card').eq(i).click(() => { window.location.href = "../pages/" + link + ".html"; })
+    $('.index-link-card').eq(i).click(() => { window.location.href = "../pages/" + link + ".html"; })
 }
